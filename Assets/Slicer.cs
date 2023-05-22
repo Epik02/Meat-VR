16748b557c7458a6458fa3cbffa43b33a6eaf3b7
Epik02 <ethan.brogly@ontariotechu.net>
2023-05-22 17:30:17 -0400

    Base Project
    -Ezyslice is implemented, objects can be cut as many times as possible
    -using temporary assets currently

---
+++ Assets/Slicer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using EzySlice;
+public class Slicer : MonoBehaviour
+{
+    public Material materialAfterSlice;
+    public LayerMask sliceMask;
+    public bool isTouched;
+    public VelocityEstimator velocityEstimator;
+
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+
+    private void Update()
+    {
+        if (isTouched == true)
+        {
+            isTouched = false;
+
+            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+            
+            foreach (Collider objectToBeSliced in objectsToBeSliced)
+            {
+                SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+
+                GameObject upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+                GameObject lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+
+                upperHullGameobject.transform.position = objectToBeSliced.transform.position;
+                lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
+
+                MakeItPhysical(upperHullGameobject);
+                MakeItPhysical(lowerHullGameobject);
+
+                //so you can cut the upper and lower hull
+                int LayerSwitch = LayerMask.NameToLayer("Sliceable");
+                upperHullGameobject.layer = LayerSwitch;
+                lowerHullGameobject.layer = LayerSwitch;
+
+                Destroy(objectToBeSliced.gameObject);
+            }
+        }
+    }
+
+    private void MakeItPhysical(GameObject obj)
+    {
+        obj.AddComponent<MeshCollider>().convex = true;
+        obj.AddComponent<Rigidbody>();
+    }
+
+    private SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
+    {
+        return obj.Slice(transform.position, transform.up, crossSectionMaterial);
+    }
+
+
+}
