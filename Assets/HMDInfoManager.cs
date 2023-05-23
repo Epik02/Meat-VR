c9e16b24821f8f370e8abbcb180662cb98258890
Robbie Strauch <robbie.strauch@gmail.com>
2023-05-22 23:30:00 -0400

    Non Oculus Mode + Locomotion Teleport

---
+++ Assets/HMDInfoManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInfoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!XRSettings.isDeviceActive)
+        {
+            Debug.Log("No Headset Plugged In");
+        }
+        else if (XRSettings.isDeviceActive &&
+            (XRSettings.loadedDeviceName == "Mock HMD" || XRSettings.loadedDeviceName == "MockHMDDisplay")) 
+        {
+            Debug.Log("Using Mock HMD");
+        }
+        else
+        {
+            Debug.Log("Headset Is " + XRSettings.loadedDeviceName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
