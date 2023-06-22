63b7cc894ff3aa726358e279d9f5f3f132017942
Robbie Strauch <robbie.strauch@gmail.com>
2023-06-22 17:03:11 -0400

    Sink Lever

---
+++ Assets/Scripts/KnifeStrength.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeStrength : MonoBehaviour
+{
+    public float strength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Sink.cs
+++ Assets/Scripts/Sink.cs
@@ -5,43 +5,35 @@ using UnityEngine;
 public class Sink : MonoBehaviour
 {
     public ParticleSystem waterParticle;
-
-    private new Animation animation;
-    private bool sinkWater;
+    public float minWater = 40.0f;
+    public float halfWater = 90.0f;
+    public float maxWater = 130.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        animation = GetComponent<Animation>();
-        sinkWater = false;
-        waterParticle.Pause();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!sinkWater && Input.GetMouseButton(0) && !animation.isPlaying) 
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                animation.Play("SinkOn");
-                waterParticle.Play();
-                sinkWater = true;
-            }
-        }
+        Debug.Log(transform.rotation.eulerAngles);
 
-        if (sinkWater && Input.GetMouseButton(0) && !animation.isPlaying)
+        if (transform.rotation.eulerAngles.z <= minWater) 
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                animation.Play("SinkOff");
-                waterParticle.Stop();
-                sinkWater = false;
-            }
+            var emission = waterParticle.emission;
+            emission.rateOverTime = 0.0f;
+        }
+        if (transform.rotation.eulerAngles.z >= halfWater)
+        {
+            var emission = waterParticle.emission;
+            emission.rateOverTime = transform.rotation.eulerAngles.z * 0.25f;
+        }
+        if (transform.rotation.eulerAngles.z >= maxWater)
+        {
+            var emission = waterParticle.emission;
+            emission.rateOverTime = transform.rotation.eulerAngles.z * 5;
         }
     }
 }
